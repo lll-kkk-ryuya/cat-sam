45a4dd52a0124b1e86f289326b7294a6f40791d2
Ryuya kawaguchi <ryu3124ruyu@gmail.com>
2024-08-30 13:25:16 +0900

    new

--- cat_sam/datasets/custom_dataset.py
+++ cat_sam/datasets/custom_dataset.py
@@ -1,28 +1,45 @@
-from .base import BaseDataset
-import os
-from PIL import Image
-import numpy as np
+import json
+from os.path import join
+from cat_sam.datasets.base import BinaryCATSAMDataset
 
-class CustomDataset(BaseDataset):
-    def __init__(self, split='train', data_root=None, transform=None, **kwargs):
-        super().__init__(split, data_root, transform, **kwargs)
-        # カスタムデータセット固有の初期化
-        self.img_dir = os.path.join(self.data_root, 'images')
-        self.mask_dir = os.path.join(self.data_root, 'masks')
-        self.img_list = [f for f in os.listdir(self.img_dir) if f.endswith(('.jpg', '.png'))]
+few_shot_img_dict = {
+    1: ['image1'],
+    16: ['image2', 'image3', 'image4', 'image5', 'image6', 'image7', 'image8', 'image9',
+         'image10', 'image11', 'image12', 'image13', 'image14', 'image15', 'image16', 'image17']
+}
 
-    def __len__(self):
-        return len(self.img_list)
+class CustamDataset(BinaryCATSAMDataset):
+    
+    def __init__(
+            self,
+            data_dir: str,
+            train_flag: bool,
+            shot_num: int = None,
+            label_threshold: int = 254,
+            object_connectivity: int = 8,
+            area_threshold: int = 20,
+            relative_threshold: bool = True,
+            relative_threshold_ratio: float = 0.001,
+            ann_scale_factor: int = 1,
+            noisy_mask_threshold: float = 0.0,
+            **super_args
+    ):
+        json_path = join(data_dir, 'train.json' if train_flag else 'test.json')
+        with open(json_path, 'r') as j_f:
+            json_config = json.load(j_f)
+        for key in json_config.keys():
+            json_config[key]['image_path'] = join(data_dir, json_config[key]['image_path'])
+            json_config[key]['mask_path'] = join(data_dir, json_config[key]['mask_path'])
 
-    def __getitem__(self, idx):
-        img_name = self.img_list[idx]
-        img_path = os.path.join(self.img_dir, img_name)
-        mask_path = os.path.join(self.mask_dir, img_name.replace('.jpg', '.png'))
+        if shot_num is not None:
+            assert shot_num in [1, 16], f"Invalid shot_num: {shot_num}! Must be either 1 or 16!"
+            json_config = {key: value for key, value in json_config.items() if key in few_shot_img_dict[shot_num]}
 
-        image = Image.open(img_path).convert('RGB')
-        mask = Image.open(mask_path).convert('L')
-
-        # データセット固有の前処理をここに追加
-
-        # BaseDatasetの共通処理を使用
-        return self.preparation_for_train(image, mask)
+        super(CustamDataset, self).__init__(
+            dataset_config=json_config, train_flag=train_flag,
+            label_threshold=label_threshold, object_connectivity=object_connectivity,
+            area_threshold=area_threshold, relative_threshold=relative_threshold,
+            relative_threshold_ratio=relative_threshold_ratio,
+            ann_scale_factor=ann_scale_factor, noisy_mask_threshold=noisy_mask_threshold,
+            **super_args
+        )
